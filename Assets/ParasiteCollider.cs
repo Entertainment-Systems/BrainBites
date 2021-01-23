b81f659254e2f2a34f91aa8d38fc9cd9ba98b407
Ciarán Malone <ciaranmalone27@gmail.com>
2021-01-23 18:20:24 +0000

    Initial Player Controller

---
+++ Assets/ParasiteCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParasiteCollider : MonoBehaviour
+{
+    public bool CollidedToEnemy = false;
+    public GameObject CollidedEnemy;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+            CollidedToEnemy = true;
+            CollidedEnemy = collision.gameObject;
+            Debug.Log(CollidedEnemy);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Rigidbody2D rb2D;
+    private ParasiteCollider parasite;
+
+    private float thrust = 10.0f;
+
+    void Start()
+    {
+        GetPlayerBody();
+    }
+
+    private void Update()
+    {
+        if (parasite != null)
+        {
+            if( player.gameObject.GetComponent<ParasiteCollider>().CollidedToEnemy)
+            {
+                player.gameObject.GetComponent<ParasiteCollider>().CollidedToEnemy = false;
+                player = player.gameObject.GetComponent<ParasiteCollider>().CollidedEnemy;
+                GetPlayerBody();
+          
+            }
+        }
+    }
+
+    /// <summary>
+    /// For controls and pysics updates
+    /// </summary>
+    void FixedUpdate()
+    {
+        //Controls
+        if (Input.GetButton("Fire1"))
+        {
+            rb2D.AddForce(transform.right * thrust);
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            ReturnToParasite();
+        }
+    }
+
+    /// <summary>
+    /// refreshes the players attributes to the new target. ie. changing rb2D to new target rb2D
+    /// </summary>
+    private void GetPlayerBody()
+    {
+        rb2D = player.GetComponent<Rigidbody2D>();
+        if (player.gameObject.GetComponent<ParasiteCollider>() != null)
+        {
+            parasite = player.gameObject.GetComponent<ParasiteCollider>();
+        }
+        else
+        {
+            parasite = null;
+        }
+    }
+
+    /// <summary>
+    /// returns the target back to the parasite
+    /// </summary>
+    private void ReturnToParasite()
+    {
+        Debug.Log("we running");
+        player = GameObject.FindGameObjectWithTag("Player");
+        GetPlayerBody();
+    }
+
+}
