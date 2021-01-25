a5bfc8c9af34318a5ddefe4091e9c8b252cfe515
Evan McConnell <mcconnell.evan@gmail.com>
2021-01-25 13:23:22 +0000

    unlock sounds n stuff

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour
     public int enemiesLeft;
     [SerializeField] GameObject openDoor;
     [SerializeField] GameObject closeDoor;
+    [SerializeField] AudioClip openSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,7 @@ public class LevelManager : MonoBehaviour
 
     public void unlockExit()
     {
+        GetComponent<AudioSource>().PlayOneShot(openSound);
         closeDoor.SetActive(false);
         openDoor.SetActive(true);
     }
